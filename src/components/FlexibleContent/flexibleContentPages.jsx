66e856210dfc2cf7406d7d5c9f2ed53b4043d0a1
Liam Wright <liam.wright@mosaiconline.co.uk>
2024-03-10 17:31:17 +0000

    converted some flexible content block to astro components to lower js load even further

--- src/components/FlexibleContent/flexibleContentPages.jsx
+++
@@ -1,8 +0,0 @@
-import FlexibleContent from "./oldflex.jsx";
-
-const FlexibleContentPages = ({ blockData }) => {
-    const blocks = blockData;
-    return <FlexibleContent blockData={blocks} contentType="Page" />;
-};
-
-export default FlexibleContentPages;
