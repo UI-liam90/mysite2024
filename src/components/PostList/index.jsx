7b002e9fbc5f55f6cbeec810230a15cf40c45c7d
liam_wright <liam.wright@mosaiconline.co.uk>
2024-06-05 15:30:00 +0100

    added spacing calc functions

--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -1,26 +1,26 @@
 import ArchivePagination from "../ArchivePagination";
-import NewsGridItemBlock from "../NewsGridItemBlock";
+import NewsGridItemBlock from "../NewsGridItemBlock/NewsGridItemBlock.astro";
 import "./style.scss";
 
 const PostList = ({ postsData, page, path, postSize }) => {
-	return (
-		<>
-			<div className="news-grid">
-				{postsData?.nodes.map((post) => {
-					return <NewsGridItemBlock postData={post} headingType={"h2"} key={post.uri} />;
-				})}
-			</div>
+    return (
+        <>
+            <div className="news-grid">
+                {postsData?.nodes.map((post) => {
+                    return <NewsGridItemBlock postData={post} headingType={"h2"} key={post.uri} />;
+                })}
+            </div>
 
-			<ArchivePagination
-				hasPreviousPage={postsData.pageInfo.offsetPagination.hasPrevious}
-				hasNextPage={postsData.pageInfo.offsetPagination.hasMore}
-				currentPage={page}
-				totalCount={postsData.pageInfo.offsetPagination.total}
-				archivePath={path}
-				postSize={postSize}
-			/>
-		</>
-	);
+            <ArchivePagination
+                hasPreviousPage={postsData.pageInfo.offsetPagination.hasPrevious}
+                hasNextPage={postsData.pageInfo.offsetPagination.hasMore}
+                currentPage={page}
+                totalCount={postsData.pageInfo.offsetPagination.total}
+                archivePath={path}
+                postSize={postSize}
+            />
+        </>
+    );
 };
 
 export default PostList;
