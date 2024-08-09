2964a694d1ce2c59953ce4ff88f4fd1236f70ff7
liam_wright <liam.wright@mosaiconline.co.uk>
2024-08-09 16:09:42 +0100

    added new console funcs

--- src/context/nanoContext.js
+++ src/context/nanoContext.js
@@ -2,3 +2,11 @@ import { atom } from "nanostores";
 //import { persistentAtom } from "@nanostores/persistent";
 
 export const isNavOpen = atom(false);
+
+export const cmds = atom(["test"]);
+
+export const addCmds = (newValues) => {
+    var oldValues = cmds.get();
+    console.log(oldValues, newValues);
+    cmds.set([...oldValues, newValues]);
+};
